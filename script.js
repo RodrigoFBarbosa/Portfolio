a4cb260f739e8b470325acb06825aa5b22f21a76
RodrigoFBarbosa <rodrigobarbosa.p99@gmail.com>
2024-01-17 15:36:39 -0300

    fix target

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const links = document.querySelectorAll(".navegacao a");
+  const navigationLinks = document.querySelectorAll(".navegacao a");
+  const socialLinks = document.querySelectorAll(".links a");
 
-  links.forEach(function (link) {
+  navigationLinks.forEach(function (link) {
     link.addEventListener("click", function (e) {
       e.preventDefault();
-
-      const targetId = this.getAttribute("href").substring(1);
-      const targetElement = document.getElementById(targetId);
-
-      if (targetElement) {
-        let offset = window.innerHeight / 2 - targetElement.offsetHeight / 2;
-
-        // Ajuste específico para a seção "sobremim"
-        if (targetId === "sobremim") {
-          offset = window.innerHeight / 4; //ajustar esse valor conforme necessário
-        }
-
-        window.scrollTo({
-          top: targetElement.offsetTop - offset,
-          behavior: "smooth",
-        });
-      }
+      scrollToTarget(link);
     });
   });
+
+  socialLinks.forEach(function (link) {
+    link.addEventListener("click", function () {
+
+      window.open(link.href, '_blank');
+    });
+  });
+
+  function scrollToTarget(link) {
+    const targetId = link.getAttribute("href").substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      let offset = window.innerHeight / 2 - targetElement.offsetHeight / 2;
+
+      // Ajuste específico para a seção "sobremim"
+      if (targetId === "sobremim") {
+        offset = window.innerHeight / 4; //ajustar esse valor conforme necessário
+      }
+
+      window.scrollTo({
+        top: targetElement.offsetTop - offset,
+        behavior: "smooth",
+      });
+    }
+  }
 });
